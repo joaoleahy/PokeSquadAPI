818187134898ea391e402d3aa6b5eececca67fda
João Leahy <joaovictorleahy.hrzon@gmail.com>
2024-03-03 20:21:25 -0300

    fix(urls): remove duplicate API prefix in TeamRetrieveView route definition

--- pokemon_squads/api/urls.py
+++ pokemon_squads/api/urls.py
@@ -4,5 +4,5 @@ from .views import TeamListView, TeamRetrieveView, TeamCreateView
 urlpatterns = [
     path('teams/', TeamListView.as_view(), name='team-list'),
     path('teams/create/', TeamCreateView.as_view(), name='team-create'),
-    path('api/teams/<str:user>/', TeamRetrieveView.as_view(), name='retrieve-team'),
+    path('teams/<str:user>/', TeamRetrieveView.as_view(), name='retrieve-team'),
 ]
