fe81eff33857b4a07d306d946b88aafd06f6c2a6
João Leahy <joaovictorleahy.hrzon@gmail.com>
2024-03-03 11:08:43 -0300

    feat: define Team and Pokemon models

--- pokemon_squads/models.py
+++ pokemon_squads/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Team(models.Model):
+    user = models.CharField(max_length=100, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Pokemon(models.Model):
+    team = models.ForeignKey(Team, related_name='pokemons', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    poke_id = models.IntegerField()
+    height = models.IntegerField()
+    weight = models.IntegerField()
