167a0936a0344ab335ec404cc95e5d70db6bd472
João Leahy <joaovictorleahy.hrzon@gmail.com>
2024-03-03 12:19:04 -0300

    feat: implement pokemon retrieval from PokeAPI in DjangoORMTeamRepository

--- pokemon_squads/infrastructure/pokemon_api.py
+++ pokemon_squads/infrastructure/pokemon_api.py
@@ -1,15 +1,17 @@
 import requests
 
-def get_pokemon_data(pokemon_name):
-    url = f'https://pokeapi.co/api/v2/pokemon/{pokemon_name}'
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
-        return {
-            'poke_id': data['id'],
-            'name': data['name'],
-            'height': data['height'],
-            'weight': data['weight']
-        }
-    else:
-        return None
+class PokeAPI:
+    BASE_URL = 'https://pokeapi.co/api/v2/pokemon/'
+
+    def get_pokemon_data(self, name):
+        response = requests.get(f"{self.BASE_URL}{name}")
+        if response.status_code == 200:
+            data = response.json()
+            return {
+                'poke_id': data['id'],
+                'name': data['name'],
+                'height': data['height'],
+                'weight': data['weight']
+            }
+        else:
+            return None
--- pokemon_squads/infrastructure/repositories.py
+++ pokemon_squads/infrastructure/repositories.py
@@ -2,10 +2,17 @@ from typing import List, Dict
 
 from pokemon_squads.domain.repositories import TeamRepository
 from pokemon_squads.models import Team, Pokemon
+from pokemon_squads.infrastructure.pokemon_api import PokeAPI
 
 class DjangoORMTeamRepository(TeamRepository):
-    def add_team(self, user: str, pokemons_data: List[Dict]) -> Team:
+    def add_team(self, user: str, pokemon_names: List[str]) -> Team:
         team = Team.objects.create(user=user)
-        for pokemon_data in pokemons_data:
-            Pokemon.objects.create(team=team, **pokemon_data)
+        poke_api = PokeAPI()
+
+        for name in pokemon_names:
+            pokemon_data = poke_api.get_pokemon_data(name)
+            if pokemon_data:
+                Pokemon.objects.create(team=team, **pokemon_data)
+            else:
+                print(f"Pokemon {name} not found in PokeAPI.")
         return team
